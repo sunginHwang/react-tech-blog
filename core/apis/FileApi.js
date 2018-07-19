a2767a2eb66f957fe71c92398035c7c1d646384a
HwangSungIn <gommpo111@gmail.com>
2018-07-19 19:28:55 +0900

    이미지 버튼 클릭으로 저장 로직 성공.

---
+++ core/apis/FileApi.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const tempPhotoServerUrl = 'http://www.woolta.com:81';
+
+export const saveImageAndGetImageUrl = async (imageFile) => {
+    let data = new FormData();
+    data.append("file", imageFile);
+
+    try{
+        const result = await axios.post(`${tempPhotoServerUrl}/api/board/insert_image`,data, {
+            withCredentials: false,
+            onUploadProgress: (e) => {
+                if (window.nanobar) {
+                    window.nanobar.go(e.loaded / e.total * 100);
+                }}
+        });
+        const { state, img_url } = await result.data;
+        await console.log(`${tempPhotoServerUrl}/uploads/${img_url}`);
+        if(state !== 'success'){
+            return await '12';
+        }else{
+            return await `${tempPhotoServerUrl}/uploads/${img_url}`;
+        }
+    }catch (e){
+        return await '34';
+    }
+
+};
--- pages/main.js
+++ pages/main.js
@@ -3,6 +3,8 @@ import Mainlayout from '../component/common/layout/Mainlayout';
 import ReactMarkdown from 'react-markdown';
 import CodeBlock from '../component/editer/CodeBlock';
 import React from "react";
+import * as FileApi from '../core/apis/FileApi';
+
 import style from '../style/scss/Main.scss';
 
 class main extends React.Component {
@@ -17,7 +19,22 @@ class main extends React.Component {
     stateChange(content){
         this.setState({content: content});
     }
+     imageUploadClick = async () => {
+        const fileInput = document.createElement('input');
+        fileInput.type = 'file';
+        fileInput.onchange = (e) => {
+            if (!fileInput.files) return;
+            FileApi.saveImageAndGetImageUrl(fileInput.files[0]).then(async(imgUrl)  => {
+                if(imgUrl ==='') return;
+                await this.stateChange(this.state.content+this.convertImageToCodeImage(imgUrl));
+            });
+        };
+        fileInput.click();
+    };
 
+    convertImageToCodeImage(imageUrl){
+        return `${'\n'}![${imageUrl}](${imageUrl})${'\n'}`;
+    }
 
     render() {
 
@@ -27,6 +44,7 @@ class main extends React.Component {
                     <span>스마트 마크다운 에디터 스터디</span>
                     <br/>
                     <br/>
+                    <button onClick={this.imageUploadClick}>이미지 업로드</button>
                     <div className={style.markDownWrapper}>
                         <textarea onChange={(e)=>{this.stateChange(e.target.value);}}>{this.state.content}</textarea>
                     </div>
