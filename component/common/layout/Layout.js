4b27e780a9aa8c616543eba7472fcddbe8549bfc
sunginHwang <gommpo111@gmail.com>
2018-07-27 23:54:51 +0900

    -- 작업내역 사이드바 링크 대체 작업

--- component/common/layout/Layout.js
+++ component/common/layout/Layout.js
@@ -60,31 +60,6 @@ export default class ReduxExampleContainer extends React.Component {
                     showMobileHeader={this.state.showMobileHeader}
                     onClickSideBar={(e) => this.onClickSideBar()}
                 />
-                {/*<div>
-                    <Link href='/write'>
-                        <button className={style.tag}>글쓰기 페이지 이동</button>
-                    </Link>
-                    <Link  href='/reduxExample'>
-                        <button className={style.tag}>리덕스 테스트 연동 테스트</button>
-                    </Link>
-                    <Link prefetch href='/reduxExample'>
-                        <button className={style.tag}>리덕스 테스트 연동 테스트 prefetch</button>
-                    </Link>
-                    <Link href='/urlRouterExample?id=sungin'>
-                        <button className={style.tag}>라우트 연동 테스트 Link 태그</button>
-                    </Link>
-                    <Link href='/custom/routes/sungin'>
-                        <button className={style.tag}>커스텀 라우트 연동 테스트 Link 태그</button>
-                    </Link>
-
-                    <Link href='/urlWithFunctionComponent?id=sunginFunction'>
-                        <button className={style.tag}>라우트 연동 테스트 a 태그 함수형컴포넌트</button>
-                    </Link>
-
-                    <Link href='/dynamicImportExample'>
-                        <button className={style.tag}>Dynamic Import Example</button>
-                    </Link>
-                </div>*/}
                 <PageLoading loading={this.state.pageLoading}/>
                 <div className={style.contentArea}>
                     {
--- component/mainTemplate/SideBar/SideBar.js
+++ component/mainTemplate/SideBar/SideBar.js
@@ -4,12 +4,27 @@ export default ({isOpen}) => (
     <div>
         <div className={style.sideBar +' '+ ( isOpen ? style.sideBarOpen: '')}>
             <ul>
-                <li>게시판1212</li>
-                <li>게시판1212</li>
-                <li>게시판1212</li>
-                <li>게시판1212</li>
-                <li>게시판1212</li>
-                <li>게시판1212</li>
+                <li>
+                    <Link href='/write'>글쓰기 페이지 이동</Link>
+                </li>
+                <li>
+                    <Link  href='/reduxExample'>리덕스 테스트 연동 테스트</Link>
+                </li>
+                <li>
+                    <Link prefetch href='/reduxExample'>리덕스 테스트 연동 테스트 prefetch</Link>
+                </li>
+                <li>
+                    <Link href='/urlRouterExample?id=sungin'>라우트 연동 테스트 Link 태그</Link>
+                </li>
+                <li>
+                    <Link href='/custom/routes/sungin'>커스텀 라우트 연동 테스트 Link 태그</Link>
+                </li>
+                <li>
+                    <Link href='/urlWithFunctionComponent?id=sunginFunction'>라우트 연동 테스트 a 태그 함수형컴포넌트</Link>
+                </li>
+                <li>
+                    <Link href='/dynamicImportExample'>Dynamic Import Example</Link>
+                </li>
             </ul>
 
         </div>
