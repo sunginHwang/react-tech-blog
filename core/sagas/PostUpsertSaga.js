514fa5b81e097996648cadd8e1d1c852c51161d5
sunginHwang <gommpo111@gmail.com>
2018-08-16 23:31:56 +0900

    -- 작업내역
    1. 액션 호출 포인트에서 기존과 같이 async-await 처리로 하려고 했으나
    아무리 방법을 찾아도 실패.
    yield 의 특성을 따라 찾아 본 결과 saga로 작업 시 success or failure 후 처리 등 모든걸 Saga에서 끝내더라.
    
    이러고 보니 의문이 생기는건 그럼 successFunc, failFunc redux 호출 포인트가 얘매?
    -> 결과값을 통해 성공이면 바로 어떤 작업을 수행 하는것도, success redux 호출 하는 놈도 있다. 즉 사용자 맘대로
    
    내일은 해당 패턴을 사용한 적용 작업 및 에러 창 만들자

--- core/sagas/PostUpsertSaga.js
+++ core/sagas/PostUpsertSaga.js
@@ -1,14 +1,24 @@
-import {call, all, takeLatest } from "redux-saga/effects";
+import {call, all, takeLatest, put} from "redux-saga/effects";
 import * as BlogAction from "../actions/BlogAction";
-import { asyncSaga } from '../util/reduxUtil';
 import * as BlogApi  from '../apis/BlogApi';
+import * as SampleApi  from '../apis/SampleApi';
 
-function * asyncRequestSaga(info) {
-    yield call(asyncSaga,BlogAction.upsertPost, BlogApi.upsertPost, info.payload);
+import Router from "next/router";
+
+
+function * postUpsertSaga(info) {
+    yield put(BlogAction.upsertPost.request()); // 요청대기
+    try {
+        const json = yield call(BlogApi.upsertPost, info.payload); // 비동기처리 promise
+        yield put(BlogAction.upsertPost.success(json)); // 비동기 처리 성공
+        yield put(Router.push(`/postDetail?postNo=1&categoryNo=1`, `/categories/1/posts/1`));
+    } catch(error) {
+        yield put(BlogAction.upsertPost.failure(error)); // 비동기 처리 실패
+    }
 }
 
 export default function* root() {
     yield all([
-        takeLatest(BlogAction.POST_UPSERT.INDEX, asyncRequestSaga) // asyncCall
+        takeLatest(BlogAction.POST_UPSERT.INDEX, postUpsertSaga) // asyncCall
     ]);
 }
