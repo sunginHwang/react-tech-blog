79989200a34a4bb919073793a31ceb8bbdff60fc
HwangSungIn <gommpo111@gmail.com>
2018-09-09 18:56:16 +0900

    작업내역 1. 헤더 스크롤 버그 및 meta태그 작업

--- pages/_document.js
+++ pages/_document.js
@@ -10,10 +10,13 @@ export default class MyDocument extends Document {
                 <link rel="icon" href="/static/woolta.ico" type="icon" />
                 <link rel="apple-touch-icon" href="/static/woolta.ico"/>
                 <link rel="shortcut icon" href="/static/woolta.ico"/>
+                <meta name="mobile-web-app-capable" content="yes"/>
+                <meta name="apple-mobile-web-app-capable" content="yes"/>
+                <meta name='description' content='woolta 블로그 ver2.0 기술 개발 블로그'/>
+                <meta property="og:description" content="woolta 블로그 ver2.0 기술 개발 블로그"/>
+                <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover'/>
                 <link rel="stylesheet" href="/_next/static/style.css" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/atelier-cave-dark.min.css"/>
-                <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover'/>
-                <meta name='description' content='woolta 블로그 ver2.0 기술 개발 목적인 블로그 입니다.'/>
 
             </Head>
             <body>
