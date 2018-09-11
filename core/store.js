e2aaca1b3f76022c4211a363e5b7cdfbc7aef82f
HwangSungIn <gommpo111@gmail.com>
2018-09-11 23:55:26 +0900

    리덕스 production 환경 변경

--- core/store.js
+++ core/store.js
@@ -1,20 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension/logOnlyInProduction';
 import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
 import rootSaga from './sagas';
 
-export function initializeStore (initialState ) {
+const isProd = process.env.NODE_ENV === 'production';
+
+export function initializeStore(initialState) {
     const sagaMiddleware = createSagaMiddleware();
 
-    const store = createStore(
-        reducers,
-        composeWithDevTools(
-            applyMiddleware(
-                sagaMiddleware
-            )
-        )
-    );
+    const middleware = isProd ? applyMiddleware(sagaMiddleware)
+        : composeWithDevTools(applyMiddleware(sagaMiddleware));
+
+    const store = createStore(reducers,middleware);
     sagaMiddleware.run(rootSaga);
     return store;
 };
