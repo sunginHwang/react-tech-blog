8a59d5f7090c2343239eb73ec3e0b69b0fe988ca
sunginHwang <gommpo111@gmail.com>
2018-09-15 17:32:07 +0900

    리스팅 컴포넌트 최적화 작업 및 es-lint dev 추가 작업

--- component/post/list/PostItem/PostItem.js
+++ component/post/list/PostItem/PostItem.js
@@ -1,24 +1,30 @@
 import style from './PostItem.scss';
-import React from "react";
+import React, {Component} from "react";
 
-export default ({
-                    title,
-                    postNo,
-                    contents,
-                    author,
-                    date,
-                    categoryNo,
-                    categoryLabel,
-                    clickDetailPage
-                }) =>
-    <div className={style.postItem}>
-        <h1 className={style.title} onClick={()=>{clickDetailPage(categoryNo, postNo)}}>{title}</h1>
-        <p className={style.content} onClick={()=>{clickDetailPage(categoryNo, postNo)}}>{contents}</p>
-        <div>
-            <span className={style.meta +' '+ style.categoryLabel}>{categoryLabel}</span>
-            <span className={style.separator}>|</span>
-            <span className={style.meta}>{author}</span>
-            <span className={style.separator}>|</span>
-            <span className={style.meta}>{date}</span>
-        </div>
-    </div>
+class PostItem extends React.Component {
+
+    shouldComponentUpdate(nextProps, nextState){
+        const isUpdate = this.props.title ===  nextProps.title && this.props.content === nextProps.content;
+        return isUpdate
+    }
+
+    render() {
+        const { title, postNo, contents, author, date,
+            categoryNo, categoryLabel, clickDetailPage } = this.props;
+        return (
+            <div className={style.postItem}>
+                <h1 className={style.title} onClick={()=>{clickDetailPage(categoryNo, postNo)}}>{title}</h1>
+                <p className={style.content} onClick={()=>{clickDetailPage(categoryNo, postNo)}}>{contents}</p>
+                <div>
+                    <span className={style.meta +' '+ style.categoryLabel}>{categoryLabel}</span>
+                    <span className={style.separator}>|</span>
+                    <span className={style.meta}>{author}</span>
+                    <span className={style.separator}>|</span>
+                    <span className={style.meta}>{date}</span>
+                </div>
+            </div>
+        )
+    }
+}
+export default PostItem;
+
--- component/post/list/PostLayout/PostLayout.js
+++ component/post/list/PostLayout/PostLayout.js
@@ -1,29 +1,36 @@
-import React from "react";
+import React, {Component} from "react";
 import PostItem from '../PostItem/PostItem';
-
 import style from './PostLayout.scss';
 
-const PostLayout = ({ posts,
-                        onClickDetailPage
-}) => {
-    const list = posts.map((post) => {
-        return <PostItem
-                  clickDetailPage={onClickDetailPage}
-                  postNo={post.postNo}
-                  key={post.postNo}
-                  title={post.title}
-                  contents={post.subDescription}
-                  categoryLabel={post.categoryLabel}
-                  categoryNo={post.categoryNo}
-                  author={post.author}
-                  date={post.createdAt}/>
-    });
-    return (
-       <div className={style.contentList}>
-           {list}
-       </div>
-    );
-};
+class PostLayout extends Component {
+
+    shouldComponentUpdate(nextProps, nextState){
+        return this.props.posts === nextProps.posts;
+    }
+
+    render() {
+        const { posts, onClickDetailPage } = this.props;
+
+        const list = posts.map((post) => {
+            return <PostItem
+                clickDetailPage={onClickDetailPage}
+                postNo={post.postNo}
+                key={post.postNo}
+                title={post.title}
+                contents={post.subDescription}
+                categoryLabel={post.categoryLabel}
+                categoryNo={post.categoryNo}
+                author={post.author}
+                date={post.createdAt}/>
+        });
+        return (
+            <div className={style.contentList}>
+                {list}
+            </div>
+        );
+
+    }
+}
 
 export default PostLayout;
 
