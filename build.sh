ac66474108478f850ac015464b8fda9196bcd419
sunginHwang <gommpo111@gmail.com>
2018-09-30 19:45:55 +0900

    codeBuild 스크립트 정의

--- build.sh
+++ build.sh
@@ -7,8 +7,9 @@ VERSION=4
 build(){
     BUILD_VERSION_NAME=${PROJECT_NAME}_${NOW}_${VERSION}
     echo "build start...."
-     zip -r "${BUILD_VERSION_NAME}.zip" .ebextensions .npmrc next.config.js package.json postcss.config.js routes.js server.js static .next
-
+    mkdir build
+    cd ./build
+    zip -r "${BUILD_VERSION_NAME}.zip" ../.ebextensions ../.npmrc ../next.config.js ../package.json ../postcss.config.js ../routes.js ../server.js ../static ../.next
 }
 
 build
