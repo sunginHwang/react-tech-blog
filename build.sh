ca06d7577325f70442a90da7d68ae9c84b964c2d
HwangSungIn <gommpo111@gmail.com>
2018-10-01 19:47:27 +0900

    S3 업로드 빌드파일 eb 매핑 작업

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 NOW=$(date +%Y%m%d%H%M%S)
 PROJECT_NAME="TechBlog"
+BEANSTALK_NAME="techBlog"
+
 VERSION=4
 S3_REPO_PATH="s3://elasticbeanstalk-ap-northeast-2-674632797140/"
 
@@ -13,7 +15,7 @@ build(){
     BUILD_FILE="${BUILD_VERSION_NAME}.zip"
     zip -r "${BUILD_FILE}" ../.ebextensions ../.npmrc ../next.config.js ../package.json ../postcss.config.js ../routes.js ../server.js ../static ../.next
 
-    s3_upload ${BUILD_FILE};
+    s3_upload ${BUILD_FILE}
 }
 
 s3_upload(){
@@ -21,6 +23,17 @@ s3_upload(){
  FILE_NAME="$1"
  OPTION=""
  aws s3 cp "${FILE_NAME}" "${S3_REPO_PATH}" ${OPTION}
+ s3_version_mapping ${FILE_NAME}
 }
 
+s3_version_mapping(){
+  S3_KEY="$1"
+
+ aws elasticbeanstalk create-application-version \
+     --application-name "${BEANSTALK_NAME}" \
+     --version-label "${PROJECT_NAME}-${NOW}" \
+     --description "${VERSION} build" \
+     --source-bundle S3Bucket="${S3_REPO_PATH}",S3Key="${S3_KEY}" \
+     --auto-create-application
+}
 build
