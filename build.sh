7d3c2994733e4ffbe614fa8a7e330dd2d35b40a6
HwangSungIn <gommpo111@gmail.com>
2018-10-01 19:39:15 +0900

    S3 업로드 빌드 쉘 작성

--- build.sh
+++ build.sh
@@ -3,13 +3,24 @@
 NOW=$(date +%Y%m%d%H%M%S)
 PROJECT_NAME="TechBlog"
 VERSION=4
+S3_REPO_PATH="s3://elasticbeanstalk-ap-northeast-2-674632797140/"
 
 build(){
     BUILD_VERSION_NAME=${PROJECT_NAME}_${NOW}_${VERSION}
     echo "build start...."
     mkdir build
     cd ./build
-    zip -r "${BUILD_VERSION_NAME}.zip" ../.ebextensions ../.npmrc ../next.config.js ../package.json ../postcss.config.js ../routes.js ../server.js ../static ../.next
+    BUILD_FILE="${BUILD_VERSION_NAME}.zip"
+    zip -r "${BUILD_FILE}" ../.ebextensions ../.npmrc ../next.config.js ../package.json ../postcss.config.js ../routes.js ../server.js ../static ../.next
+
+    s3_upload ${BUILD_FILE};
+}
+
+s3_upload(){
+
+ FILE_NAME="$1"
+ OPTION=""
+ aws s3 cp "${FILE_NAME}" "${S3_REPO_PATH}" ${OPTION}
 }
 
 build
