c1aa0ae19381cf3e6be4b4fc39cddb238e7d1e89
sunginHwang <gommpo111@gmail.com>
2018-12-23 20:28:04 +0900

    화면 변경시 스크롤 최상위 고정

--- core/util/RouteUtil.js
+++ core/util/RouteUtil.js
@@ -1,25 +1,30 @@
 import Router from "next/router";
 
 export const goPostListPage = (categoryNo) => {
+    window.scrollTo(0, 0);
     Router.push(`/postList?categoryNo=${categoryNo}`, `/categories/${categoryNo}`);
 };
 
 export const goPostDetailPage = (categoryNo, postNo) => {
+    window.scrollTo(0, 0);
     Router.push(`/postDetail?postNo=${postNo}&categoryNo=${categoryNo}`, `/categories/${categoryNo}/posts/${postNo}`);
 };
 
 export const goPostEditPage = () => {
+    window.scrollTo(0, 0);
     Router.push('/postEdit','/edit');
 };
 
 export const goMainPage = () => {
+    window.scrollTo(0, 0);
     Router.push('/','/');
 };
 
 export const goLoginPage = () => {
+    window.scrollTo(0, 0);
     Router.push('/login','/login');
 };
-
+/**/
 export const goBack = () => {
     Router.back();
 };
