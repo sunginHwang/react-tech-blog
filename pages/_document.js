1837f14041cc103dcc2fcc6ede4910eb31c2a18d
sunginHwang <gommpo111@gmail.com>
2019-05-12 15:02:12 +0900

    구글 search

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,7 @@ export default class MyDocument extends Document {
                 <meta name='description' content='woolta 블로그 ver2.0 기술 개발 블로그'/>
                 <meta property="og:description" content="woolta 블로그 ver2.0 기술 개발 블로그"/>
                 <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover'/>
+                <meta name="google-site-verification" content="zskhf8kVpb_u2sF1hCCcL4ETy6V7ZHYnsr2JNoaV5cA" />
                 <link rel="stylesheet" href="/_next/static/style.css" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/atelier-cave-dark.min.css"/>
                 <link href="https://fonts.googleapis.com/css?family=Roboto:400,700,900&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet"/>
