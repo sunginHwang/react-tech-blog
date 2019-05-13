c060642668c3e626a6ff10de2e4074553e962f43
sunginHwang <gommpo111@gmail.com>
2019-05-13 17:00:37 +0900

    robots 검색 추가

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const { createServer } = require('http')
+const express = require('express')
 const next = require('next')
 const routes = require('./routes')
 
@@ -7,12 +8,27 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handler = routes.getRequestHandler(app);
 
+const path = require('path');
+const options = {
+    root: path.join(__dirname, '/'),
+    headers: {
+        'Content-Type': 'text/plain;charset=UTF-8',
+    }
+};
 
-app.prepare()
-    .then(() => {
-        createServer(handler)
-            .listen(port, (err) => {
-                if (err) throw err
-                console.log(`> Ready on http://localhost:${port}`);
-            })
-    });
+app.prepare().then(() => {
+    const server = express()
+
+    server.get('/robots.txt', (req, res) => (
+        res.status(200).sendFile('robots.txt', options)
+    ));
+
+    server.get('*', (req, res) => {
+        return handler(req, res)
+    })
+
+    server.listen(port, err => {
+        if (err) throw err
+        console.log(`> Ready on http://localhost:${port}`)
+    })
+})
