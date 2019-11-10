2addd9e4a63d1310d3738f1a612ca08e2a763622
hwangseong-in <gommpo111@gmail.com>
2019-11-11 00:03:33 +0900

    add saga testing setup

--- test/start.test.js
+++ test/start.test.js
@@ -1,10 +1,15 @@
-
-function sum(a, b) {
-    return a + b;
-}
-
-const sum2= (a,b) => a+b
+import {sum} from '../core/util/ImageUtil';
+import {asyncSaga} from '../core/util/ReduxSagaUtil';
+import * as CategoryAction from "../store/actions/CategoryAction";
+import * as BlogApi from '../core/apis/BlogApi';
+import {call, put} from "redux-saga/effects";
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum2(1, 2)).toBe(3);
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('saga', () => {
+    const info = asyncSaga(CategoryAction.getCategories, BlogApi.getCategories, '');
+    expect(info.next().value).toBe(put(CategoryAction.getCategories.request()));
+    expect(info.next().value).toBe(put(CategoryAction.getCategories.success()));
 });
