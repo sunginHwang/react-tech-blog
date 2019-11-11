17718dbb4b9c7eea1d4afd065d0040f15babbb3c
hwangseong-in <gommpo111@gmail.com>
2019-11-11 19:13:16 +0900

    add saga test

--- test/start.test.js
+++ test/start.test.js
@@ -8,8 +8,14 @@ test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
+
+test('getCategories', () => {
+    expect(BlogApi.getCategories()).toEqual([{}]);
+});
+
 test('saga', () => {
     const info = asyncSaga(CategoryAction.getCategories, BlogApi.getCategories, '');
-    expect(info.next().value).toBe(put(CategoryAction.getCategories.request()));
-    expect(info.next().value).toBe(put(CategoryAction.getCategories.success()));
+    expect(info.next().value).toEqual(put(CategoryAction.getCategories.request()));
+    expect(info.next().value).toEqual(call(BlogApi.getCategories, ''));
+    expect(info.next().value).toEqual('');
 });
